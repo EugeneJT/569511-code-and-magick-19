a4752adf3f80b5871530f865c6fa0da924def915
Eugene Nasonov <eugenetikk@gmail.com>
2020-02-09 04:29:44 +0300

    made the edits to the module2-task

--- Readme.md
+++ Readme.md
@@ -1,7 +1,7 @@
 # Учебный проект «Код и Магия» [![Build status][travis-image]][travis-url]
 
 * Студент: [Evgeny Nasonov](https://up.htmlacademy.ru/javascript/19/user/569511).
-* Наставник: `Неизвестно`.
+* Наставник: [Алёна Сиприкова](https://htmlacademy.ru/profile/asiprikova).
 
 ---
 
--- js/stat.js
+++ js/stat.js
@@ -4,12 +4,21 @@ var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
+var CLOUD_COLOR = '#fff';
+var CLOUD_SHADOW_COLOR = 'rgba(0, 0, 0, 0.7)';
 var GAP = 10;
 var BAR_GAP = 50;
 var BAR_WIDTH = 40;
 var BAR_HEIGHT = 150;
+var MY_BAR_COLOR = 'rgba(255, 0, 0, 1)';
 var TEXT_HEIGHT = 16;
 var TEXT_GAP = 35;
+var TEXT_COLOR = '#000';
+var TEXT_FONT = '16px PT Mono';
+var TEXT_BASELINE = 'hanging';
+
+var HEADING_TEXT = ['Ура вы победили!', 'Список результатов:'];
+
 var oneItemWidth = BAR_GAP + BAR_WIDTH;
 
 var renderCloud = function (ctx, x, y, color) {
@@ -29,37 +38,34 @@ var getMaxElement = function (arr) {
   return maxElement;
 };
 
+var getRandomNum = function (min, max) {
+  return Math.floor(min + Math.random() * (max + 1 - min));
+};
+
 window.renderStatistics = function (ctx, names, times) {
 
   // Cloude
-  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, CLOUD_SHADOW_COLOR);
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, CLOUD_COLOR);
 
   // Main-text
-  ctx.fillStyle = '#000';
-  ctx.font = '16px PT Mono';
-  ctx.textBaseline = 'hanging';
-  ctx.fillText('Ура вы победили!', CLOUD_X + GAP * 2, TEXT_GAP);
-  ctx.fillText('Список результатов:', CLOUD_X + GAP * 2, TEXT_GAP * 1.5);
+  ctx.fillStyle = TEXT_COLOR;
+  ctx.font = TEXT_FONT;
+  ctx.textBaseline = TEXT_BASELINE;
+
+  for (i = 0; i < HEADING_TEXT.length; i++) {
+    ctx.fillText(HEADING_TEXT[i], CLOUD_X + GAP * 1.5, TEXT_GAP * (i + 1) * 0.9);
+  }
 
   var maxTime = getMaxElement(times);
-
-  function getColorSaturation(min, max) {
-    // a random numer from min to (max+1)
-    var rand = min + Math.random() * (max + 1 - min);
-    return Math.floor(rand);
-  }
 
   // Bar charts
 
   for (var i = 0; i < names.length; i++) {
-    if (names[i] === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    } else {
-      ctx.fillStyle = 'hsl(232,' + getColorSaturation(1, 100) + '%, 45%)';
-    }
-    ctx.fillText(names[i], CLOUD_X + BAR_GAP + oneItemWidth * i, CLOUD_HEIGHT - GAP * 2);
+    ctx.fillStyle = names[i] === 'Вы' ? MY_BAR_COLOR : 'hsl(232,' + getRandomNum(1, 100) + '%, 45%)';
     ctx.fillRect(CLOUD_X + BAR_GAP + oneItemWidth * i, CLOUD_HEIGHT - GAP * 2 - TEXT_HEIGHT, BAR_WIDTH, -(BAR_HEIGHT * times[i]) / maxTime);
+    ctx.fillStyle = TEXT_COLOR;
+    ctx.fillText(names[i], CLOUD_X + BAR_GAP + oneItemWidth * i, CLOUD_HEIGHT - GAP * 2);
   }
 };
 
