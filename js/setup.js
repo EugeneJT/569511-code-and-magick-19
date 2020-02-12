242d41b3535297c28e6879629e5bdbd803d415e0
Eugene Nasonov <eugenetikk@gmail.com>
2020-02-12 23:43:31 +0300

    fixed the edits for the lesson 3

--- js/setup.js
+++ js/setup.js
@@ -3,9 +3,9 @@
 
 var WIZARD_FIRST_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_LAST_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var WIZARD_COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var WIZARD_EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
-var counter = 4;
+var WIZARD_COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var WIZARD_EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var COUNTER = 4;
 
 
 var userDialog = document.querySelector('.setup');
@@ -22,8 +22,8 @@ var createWizards = function (count) {
   for (var i = 0; i < count; i++) {
     wizards.push({
       name: WIZARD_FIRST_NAMES[getRandomNum(0, WIZARD_FIRST_NAMES.length - 1)] + '\n' + WIZARD_LAST_NAMES[getRandomNum(0, WIZARD_LAST_NAMES.length - 1)],
-      coatColor: WIZARD_COAT_COLOR[getRandomNum(0, WIZARD_COAT_COLOR.length)],
-      eyesColor: WIZARD_EYES_COLOR[getRandomNum(0, WIZARD_EYES_COLOR.length)]
+      coatColor: WIZARD_COAT_COLORS[getRandomNum(0, WIZARD_COAT_COLORS.length)],
+      eyesColor: WIZARD_EYES_COLORS[getRandomNum(0, WIZARD_EYES_COLORS.length)]
     });
   }
   return (wizards);
@@ -39,9 +39,11 @@ var renderWizard = function (wizard) {
   return wizardElement;
 };
 
+var similarWizard = createWizards(COUNTER);
+
 var fragment = document.createDocumentFragment();
-for (var i = 0; i < createWizards(counter).length; i++) {
-  fragment.appendChild(renderWizard(createWizards(4)[i]));
+for (var i = 0; i < similarWizard.length; i++) {
+  fragment.appendChild(renderWizard(similarWizard[i]));
 }
 similarListElement.appendChild(fragment);
 
